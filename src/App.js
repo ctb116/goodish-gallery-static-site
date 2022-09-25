ec4c62701c893a8dfde02568027c7078fcc2755c
ctb116 <cathytbradley116@gmail.com>
2022-09-25 16:27:54 -0700

    add comic chapter select

--- src/App.js
+++ src/App.js
@@ -7,15 +7,48 @@ const arrayOfComics = [
   route: "this is a route 1",
   title: "this is the title 1",
   titleUrl: "https://i.imgur.com/0gZlG7Y.jpeg"
-},
-{
-  NO_ID_FIELD: "ID 2",
-  description: "this is a description 2",
-  headerUrl: "this is a header URL 2",
-  route: "this is a route 2",
-  title: "This is the title 2",
-  titleUrl: "https://i.imgur.com/Wmp3yCM.jpeg"
-}
+  },
+  {
+    NO_ID_FIELD: "ID 2",
+    description: "this is a description 2",
+    headerUrl: "this is a header URL 2",
+    route: "this is a route 2",
+    title: "This is the title 2",
+    titleUrl: "https://i.imgur.com/Wmp3yCM.jpeg"
+  }
+]
+
+const arrayOfPages = [
+  {
+    name: "the name for this page",
+    description: "The description of this page",
+    page: 1,
+    pageUrl: "https://i.imgur.com/PdiIXBe.png"
+  },
+  {
+    name: "the name for this page",
+    description: "The description of this page",
+    page: 2,
+    pageUrl: "https://i.imgur.com/eC2H1ix.png"
+  },
+  {
+    name: "the name for this page",
+    description: "The description of this page",
+    page: 3,
+    pageUrl: "https://i.imgur.com/RbkSaII.jpeg"
+  },
+  {
+    name: "the name for this page",
+    description: "The description of this page",
+    page: 4,
+    pageUrl: "https://i.imgur.com/yTvPglU.jpeg"
+  },
+  {
+    name: "the name for this page",
+    description: "The description of this page",
+    page: 5,
+    pageUrl: "https://i.imgur.com/qfkduUJ.jpeg"
+  }
 ]
 
 //Static Site 
@@ -33,21 +66,29 @@ export default function App() {
     <div>
       <h1>Main Page</h1>
       <div style={artWallDisplay}>
-        <Gallery comics={arrayOfComics}/>
+        <GalleryControl comics={arrayOfComics} pages={arrayOfPages}/>
       </div>
     </div>
   );
 }
 
-class Gallery extends React.Component {
+class GalleryControl extends React.Component {
   render() {
     const comics = this.props.comics
+    const pages = this.props.pages
     
     const comicCoverGalleryStyles = {
       display: 'grid',
       // two columns, 1 row
       gridTemplateColumns: 'repeat(2, auto [col-start])',
       gridTemplateRows: 'repeat(1, auto [row-start])',
+    }
+    //these two swap
+    const comicPageGalleryStyles = {
+      display: 'grid',
+      // two columns, 1 row
+      gridTemplateColumns: 'repeat(4, auto [col-start])',
+      gridTemplateRows: 'repeat(2, auto [row-start])',
     }
     
     const comicCoverImg = {
@@ -58,12 +99,12 @@ class Gallery extends React.Component {
       maxHeight: '400px',
       marginLeft: 'auto',
       marginRight: 'auto',
-      objectFit: 'scale-down',
+      objectFit: 'cover',
     }
 
     const test = {
       backgroundColor: 'green',
-      margin: 'auto',
+      margin: '5px',
       padding: '5px',
       outlineStyle: 'double',
       outlineWidth: 'medium',
@@ -73,16 +114,22 @@ class Gallery extends React.Component {
     const comicCoverGallery = comics.map((comic) => (
       <figure style={test}>
         <GalleryTitle title={comic.title}/>
-        <GalleryImage titleUrl={comic.titleUrl} imgStyle={comicCoverImg}/>
+        <GalleryImage url={comic.titleUrl} imgStyle={comicCoverImg}/>
         <GalleryDescription description={comic.description}/>
+      </figure>
+    ))
+
+    const comicPageGallery = pages.map((page) =>(
+      <figure style={test}>
+        <GalleryImage url={page.pageUrl} imgStyle={comicCoverImg}/>
       </figure>
     ))
     
     return (
       <div>
         <h2>Comics</h2>
-        <div style={comicCoverGalleryStyles} className={"grid"}>
-         {comicCoverGallery}
+        <div style={comicPageGalleryStyles} className={"grid"}>
+         {comicPageGallery}
         </div>
       </div>
     );
@@ -94,7 +141,7 @@ function GalleryTitle(props) {
 }
 
 function GalleryImage(props) {
-  return <img src={props.titleUrl} style={props.imgStyle}/>
+  return <img src={props.url} style={props.imgStyle}/>
 }
 
 function GalleryDescription(props) {
