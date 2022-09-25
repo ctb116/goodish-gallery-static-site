bf5dce182a0dae87b6c697fe5312f84f575597c3
ctb116 <cathytbradley116@gmail.com>
2022-09-24 17:47:58 -0700

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # goodish-gallery-static-site
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-r7tj9z)
+
+
+What does your data look like?
+
+
+using fat arrow for functional component because I prefer the syntax. There is some but insignifiant different between far arrow and normal function
--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
+// fake data
+const listOfComics = [
+  {
+  NO_ID_FIELD: "ID 1",
+  description: "this is a description 1",
+  headerUrl: "this is a header URL 1 - the cropped image may not need this",
+  route: "this is a route 1",
+  title: "this is the title 1",
+  titleUrl: "https://i.imgur.com/Wmp3yCM.jpeg"
+  },
+  {
+  NO_ID_FIELD: "ID 2",
+  description: "this is a description 2",
+  headerUrl: "this is a header URL 2",
+  route: "this is a route 2",
+  title: "https://i.imgur.com/Wmp3yCM.jpeg",
+  titleUrl: "https://i.imgur.com/Wmp3yCM.jpeg"
+  }
+]
+
+//Static Site 
 import React from "react";
 import "./style.css";
+
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Main Page</h1>
+      <ComicWall comics={listOfComics}/>
     </div>
   );
 }
+
+
+// really this just displays an image. How that image is displayed could change on the CSS
+// responsible for a grid of images
+class ComicWall extends React.Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h2>This is a list of comics to pick from</h2>
+        {this.props.comics.forEach((comic) => {
+          <Comic/>
+        })
+        }
+      </div>
+    );
+  }
+}
+
+const Comic = (props) => {
+  console.log(props)
+  return (
+    <div>
+      hi
+
+    </div>
+  )
+}
+
+// this.props.products.forEach((product) => {
+//   }
